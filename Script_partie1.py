2dbea6c00691378e2a832c4e91c0e389eb8bcec6
Laura Desjacques <desjacques.lauralee@gmail.com>
2024-10-25 14:20:02 +0200

    partie 1

---
+++ Script_partie1.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# URL de la page à scraper
+url = 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html'
+
+# Faire la requête HTTP
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Trouver le tableau des statistiques
+table = soup.find('table', {'id': 'per_game_stats'})
+
+# Extraire les en-têtes
+headers = []
+for th in table.find('thead').find_all('th'):
+    headers.append(th.text)
+
+
+# Extraire les lignes de données
+data = []
+for row in table.find_all('tr')[1:]: # Ignorer la première ligne (en-tête)
+    cols = row.find_all(['th','td'])
+    if cols:  # S'assurer que la ligne contient des données
+        data.append([col.text for col in cols])
+
+
+# Créer un DataFrame
+df = pd.DataFrame(data=data, columns=headers)
+
+# Exporter le DataFrame en CSV
+df.to_csv('./scraped_files/NBA_player_stats_2022.csv', index=False)
+
+print("Données extraites et enregistrées dans le dossier scraped_files.")
